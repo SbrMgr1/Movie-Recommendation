bac5e4e6cbbf5cb8809b22badac31035158cdabc
sbr mgr <sbr.mgr1@gmail.com>
2020-05-06 00:01:32 -0500

    managed some codes

--- vue-src/src/components/Header.vue
+++ vue-src/src/components/Header.vue
@@ -10,10 +10,6 @@
         <li>
           <router-link to="/cms/privacy-policy">Now On Cinema</router-link>
         </li>
-
-        <li>
-          <router-link to="/ItemDetails">ItemDetails</router-link>
-        </li>
         <li>
           <router-link to="/about">About</router-link>
         </li>
