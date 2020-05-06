f896015eed5d0f4c3e9e8aa8adb59783e5be5511
sbr mgr <sbr.mgr1@gmail.com>
2020-05-05 20:56:24 -0500

    Removed 10 for user recommendation label

--- vue-src/src/components/Home.vue
+++ vue-src/src/components/Home.vue
@@ -7,7 +7,7 @@
           </div>
       </div>
       <div class="container-fluid">
-          <h3 class="col-sm-12" v-if="userTopMovies.length>0">Top 10 Recommendations</h3>
+          <h3 class="col-sm-12" v-if="userTopMovies.length>0">Top Recommendations For You</h3>
           <div class="col-sm-2 mb-30" v-for="(userTopMovie,index) in userTopMovies" :key="index">
               <router-link :to="{path: '/details/' + userTopMovie.movieId}"><img class="movie-image img-responsive" :title="userTopMovie.title" :src="userTopMovie.url"></router-link>
           </div>
